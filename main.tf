1440dfd7d233911b28259da68cdfbb77ad9692fc
rizwan <rizwanwills@gmail.comm>
2024-08-18 19:46:42 +0530

    code sent

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "rg-007" {
   name = "lol"
   location = "canadacentral"
 }
+
+
+resource "azurerm_resource_group" "rg-009" {
+  name = "lol-09"
+  location = "canadacentral"
+}
+
+resource "azurerm_resource_group" "rg-10" {
+  name = "lol-10"
+  location = "canadacentral"
+}
