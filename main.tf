3b4595d996253df855d6d3f56e03b87837f80f0b
rizwan <rizwanwills@gmail.comm>
2024-08-26 20:15:07 +0530

    added line and delete line

--- main.tf
+++ main.tf
@@ -8,8 +8,4 @@ resource "azurerm_resource_group" "rg-009" {
   name = "lol-09"
   location = "canadacentral"
 }
-
-resource "azurerm_resource_group" "rg-10" {
-  name = "lol-10"
-  location = "canadacentral"
-}
+I am rizwan
